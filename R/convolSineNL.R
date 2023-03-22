b8f373c28837ac9a0b9e744776b9372393322bbc
nguyenta <tam.nguyen@ufz.de>
2023-03-22 20:21:22 +0100

    add convolution function

--- R/convolSineNL.R
+++ R/convolSineNL.R
@@ -1,42 +1,103 @@
+#' Convolve sine wave isotope concentration in precipitation using gamma function
+#'
+#' @param AP Amplitude of the sine wave (cP = AP*sin(2*pi*t - phiP) + kP) of
+#' isotope concentration in precipitation.
+#' @param phiP Phase shift of the sine wave (cP = AP*sin(2*pi*t - phiP) + kP) of
+#' isotope concentration in precipitation.
+#' @param kP The constant factor of sine wave (cP = AP*sin(2*pi*t - phiP) + kP)
+#' of isotope concentration in precipitation.
+#' @param estAlpha The 'user' estimated alpha (shape) of the gamma distribution
+#' function used for the convolution approach
+#' @param estBeta The estimated beta (scale) of the gamma distribution function
+#' use for the convolution approach
+#' @param simulatedDate The date vector (must be in format "YYYY-MM-DD") and as
+#' DATE of the time isotope concentration in streamflow should be estimated
+#' @param nWarmupYears Number of warm-up years before the starting date of the
+#' simulatedDate. This should be a few years (e.g.,5 years) to remove the effect
+#' of initial condition on the estimated results.
+#' @param printAll A logical variable, if TRUE then the results (simulated isotope
+#' concentration in streamflow) is printed for the entire simulation period,
+#' including the warm up period. If FALSE then the results is only for the
+#' simulatedDate
+#' @return A data frame of the date and simulated instream isotope concentration
+#' @details This function convolute the input sine wave (cP = AP*sin(2*pi*t - phiP)
+#' + kP) with the given gamma distribution (user given the shape and scale factor)
+#' and return the simulated isotope concentrations in streamflow
+#' @references
+#' Kirchner, J. W. (2016). Aggregation in environmental systems – Part 1: Seasonal
+#' tracer cycles quantify young water fractions, but not mean transit times,
+#' in spatially heterogeneous catchments, Hydrol. Earth Syst. Sci., 20, 279–297,
+#' https://doi.org/10.5194/hess-20-279-2016.
+#' @examples
+#' simC <- convolSineNL(AP = 1.909, phiP = 2.016, kP = -11.05995, estAlpha = 0.802,
+#'                      estBeta = 0.127, simulatedDate = isotopeS$date,
+#'                      nWarmupYears = 5, printAll = FALSE)
+#' @export
 
-
-convolSineNL <- function(sDate = NULL, eDate = NULL, AP = NULL,
-                         phiP = NULL, kP = NULL, shape = NULL,
-                         scale = NULL, isotopeS = NULL, weights = NULL){
+convolSineNL <- function(AP = NULL, phiP = NULL, kP = NULL, estAlpha = NULL,
+                         estBeta = NULL, simulatedDate = NULL,
+                         nWarmupYears = NULL, printAll = FALSE){
 
   # Sine function in form c = A sin (2*pi*t - phi) + k
-  SineNL <- function(t, A, phi, k){
-    A * sin(2*pi*t - phi) + k
+  sineNL <- function(time, AP, phiP, kP){
+    AP * sin(2*pi*time - phiP) + kP
   }
 
-  tRange <- seq(from = as.Date("2010-01-01", format = "%Y-%m-%d"),
-                to = as.Date("2020-12-31", format = "%Y-%m-%d"),
-                by = "days")
-  tRangeAbs <- decimal_date(tRange)
-  tRangeDec <- tRangeAbs - trunc(tRangeAbs)
-  tSRangeAbs <- decimal_date(isotopeS$date)
-  tSLoc <- which(tRangeAbs %in% tSRangeAbs)
-  tRangeFromOrigin <- tRangeAbs- tRangeAbs[1]
-  ntimeStep <- length(tRangeAbs)
-
-  # Get cummulative concentration using cummulative gamma
-  isoC <- SineNL(tRangeDec, A = 1.909, phi = 2.016, k = -11.05995)
-  plot(x = tRangeAbs, y = isoC, type = "l")
-  convoluteC <- rep(0,ntimeStep)
-
-  for (i in 1:ntimeStep){
-    simC <- isoC[i] * pgamma(tRangeFromOrigin, shape = 0.802,
-                             scale = 0.127, lower = TRUE)
-    simC <- diff(c(0,simC))
-    convoluteC[i:ntimeStep] <- convoluteC[i:ntimeStep] + simC[1:(ntimeStep - i + 1)]
+  # check input data is null
+  if (is.null(simulatedDate) | is.null(AP) | is.null(phiP) | is.null(kP) |
+      is.null(estAlpha)| is.null(estBeta)){
+    stop("One of the input variable is NULL")
   }
 
-  #Calculate mean square
-  plot(x = tRangeAbs, y = convoluteC, type = "l", ylim = c(-20,-5))
-  lines(x = tRangeAbs[tSLoc], y = isotopeS$O18)
+  # check if nWarumUpYears is too short
+  if (nWarmupYears < 5){
+    stop("nWarmupYears should be larger than 5 to avoid effect of initial conditions")
+  }
+
+  # simulation date
+  simDate <- seq(from = simulatedDate[1] - nWarmupYears*365,
+                 to = simulatedDate[length(simulatedDate)],
+                 by = "days")
+
+  # Number of simulation dates
+  nDates <- length(simDate)
+
+  # Convert SIMulation DATE to DECimal e.g., 2001.00, 2001.99
+  simDateDec <- lubridate :: decimal_date(simDate)
+
+  # Simulation date in decimal but starting date is 0
+  simDateDec0 <- simDateDec- simDateDec[1]
+
+  # Convert OBServed DATE to DECimal
+  obsDateDec <- lubridate :: decimal_date(simulatedDate)
+
+  # Find Location of COMmon DATE between simDateDec and obsDateDec
+  comDateLoc <- which(simDateDec %in% obsDateDec)
+
+  # Convert simulation date to range of [0,1]
+  simDate01 <- simDateDec - trunc(simDateDec)
+
+  # Find the observed isotope in precipitation (estimated from sineNL functoin )
+  obsIsoPre <- sineNL(simDate01, AP = AP, phiP = phiP, kP = kP)
+  #plot(x = simDateDec, y = obsIsoPre, type = "l")
 
 
-  # Young water fraction 0.2 years
-  pgamma(0.2, shape = 0.802, scale = 0.127, lower = TRUE)
+  # Initialized the CONVOLuted ISOtope CONCentration
+  convolIsoConc <- rep(0,nDates)
+
+  for (i in 1:nDates){
+    simIsoConc <- obsIsoPre[i] * pgamma(simDateDec0, shape = estAlpha,
+                                        scale = estBeta, lower = TRUE)
+    simIsoConc <- diff(c(0,simIsoConc))
+    convolIsoConc[i:nDates] <- convolIsoConc[i:nDates] + simIsoConc[1:(nDates - i + 1)]
+  }
+
+  if(printAll){
+    output <- data.frame(date = simDate, simIsoConc = convolIsoConc)
+  } else {
+    output <- data.frame(date = simulatedDate,simIsoConc = convolIsoConc[comDateLoc])
+  }
+
+  return(output)
 
 }
