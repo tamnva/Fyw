5c0c689064a2cd706f0269cb99f877b0e31e22b0
nguyenta <tam.nguyen@ufz.de>
2023-04-17 23:25:30 +0200

    update documentation

--- R/findPhi.R
+++ R/findPhi.R
@@ -1,29 +1,52 @@
 #' Find \eqn{\varphi} within the range of \eqn{[0, 2\pi]}
 #'
-#' @param aP A scalar or vector of the shape factor of the gamma distribution
-#' @param bP A scalar or vector of the scale factor of the gamma distribution
-#' phiS MUST be smaller than phiP
-#' @return A single phiP value within the range of \eqn{[0, 2\pi]}
-#' @details This function finds root of the following system of equations for \eqn{\varphi_P}
-#' that within the range of \eqn{[0, 2\pi]}{} given values of aP and bP \cr
-#' \deqn{a_P = - \sqrt{a_P^2 + b_P^2} \cdot sin(\varphi_P) = -A_p \cdot sin(\varphi_P)}{}
-#' \deqn{b_P = \sqrt{a_P^2 + b_P^2} \cdot cos(\varphi_P) = A_p \cdot cos(\varphi_P))}{}
+#' @param aP A first scalar (see Details)
+#' @param bP A second scalar (see Details)
+#'
+#' @return A single phiP value within the range of \eqn{[0, 2\pi]}.
+#'
+#' @details
+#' Given the sine function: \cr
+#' \deqn{c_P = A_P \cdot sin(2 \pi t - \varphi_P) + k_P}{}
+#'
+#' which can be rewritten as follows \cr:
+#'  \deqn{c_P = a_P \cdot cos(2 \pi t) + b_P \cdot sin(2 \pi t) + k_P}{}
+#'
+#' where \cr:
+#' \deqn{a_P = -A_p \cdot sin(\varphi_P)}{}
+#' \deqn{b_P = A_p \cdot cos(\varphi_P)}{}
+#'
+#' Assuming that \eqn{a_P, b_P} are known, find \eqn{\varphi_P} within the range
+#' of \eqn{[0, 2\pi]}{}
+#'
 #' @references
-#' Kirchner, J. W. (2016). Aggregation in environmental systems – Part 1: Seasonal
-#' tracer cycles quantify young water fractions, but not mean transit times,
-#' in spatially heterogeneous catchments, Hydrol. Earth Syst. Sci., 20, 279–297,
-#' https://doi.org/10.5194/hess-20-279-2016.
+#' Kirchner, J. W. (2016). Aggregation in environmental systems –
+#' Part 1: Seasonal tracer cycles quantify young water fractions, but not mean
+#' transit times, in spatially heterogeneous catchments. \emph{Hydrol. Earth Syst. Sci.},
+#' 20, 279–297. \url{https://doi.org/10.5194/hess-20-279-2016}.
+#'
 #' @examples
 #' Ap <- 2.5
 #' phiP <- 0.1
+#'
 #' findPhi(aP = -Ap*sin(phiP), bP = Ap*cos(phiP))
+#'
 #' # This function should return phiP = 0.1
+#'
 #' phiP <- 2*pi - 0.1
+#'
 #' findPhi(aP = -Ap*sin(phiP), bP = Ap*cos(phiP))
+#'
 #' # This function should return phiP = 2*pi - 0.1
+#'
 #' @export
 
 findPhi <- function(aP = NULL, bP = NULL){
+
+  # Check if input is null
+  if (is.null(aP) | is.null(bP)) stop("aP and bP cannot be NULL")
+
+  # Find first value of phi
   phiP <- atan(-aP/bP)
 
   # if phiP is negative then get positive value
---
+++ vignettes/Fyw_traditional_procedure.R
@@ -0,0 +1,83 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup, message=FALSE-----------------------------------------------------
+# Load require packages
+library(Fyw)
+library(lubridate)
+
+## ---- message=FALSE-----------------------------------------------------------
+# Get isotope in precipitation (P) and streamflow (S) from the the Alp catchment
+isotopeP <- subset(isotopeData, catchment == "Alp" & variable == "precipitation")
+isotopeS <- subset(isotopeData, catchment == "Alp" &  variable == "streamflow")
+
+## ---- message=FALSE-----------------------------------------------------------
+#------------------------------------------------------------------------------#
+#             Fit sine wave to observed isotope in precipitation               #
+#------------------------------------------------------------------------------#
+# First, convert date to decimal, ranging from [0, 1]
+t <- decimal_date(isotopeP$date) - trunc(decimal_date(isotopeP$date))
+  
+# Fit to sine wave to observed isotope in P using IRLS method
+sineP <- IRLS(Y = isotopeP$delta_18O,
+              X = data.frame(cos = cos(2*pi*t), sin = sin(2*pi*t)),
+              pweights = isotopeP$water_flux_mm)
+
+#------------------------------------------------------------------------------#
+#             Fit sine wave to observed isotope in streamflow                  #
+#------------------------------------------------------------------------------#
+# First, convert date to decimal, ranging from [0, 1]
+t <- decimal_date(isotopeS$date) - trunc(decimal_date(isotopeS$date))
+  
+# Fit to sine wave to observed isotope in P using IRLS method
+sineS <- IRLS(Y = isotopeS$delta_18O,
+              X = data.frame(cos = cos(2*pi*t), sin = sin(2*pi*t)),
+              pweights = isotopeS$water_flux_mm)
+
+
+## ---- message=FALSE-----------------------------------------------------------
+# Parameters of the fitted sine wave to isotope in precipitation
+kP <- as.numeric(sineP$coefficients[1])
+aP <- as.numeric(sineP$coefficients[2])
+bP <- as.numeric(sineP$coefficients[3])
+
+# Parameters of the fitted sine wave to isotope in precipitation
+kS <- as.numeric(sineS$coefficients[1])
+aS <- as.numeric(sineS$coefficients[2])
+bS <- as.numeric(sineS$coefficients[3])
+
+# Amplitudes of the fitted sine wave to precipitation and streamflow (please see Eq. 6; Kirchner, (2016))
+AP <- sqrt(aP^2 + bP^2)
+AS <- sqrt(aS^2 + bS^2)
+
+# Phase 
+phiP <- findPhi(aP = aP, bP = bP)
+phiS <- findPhi(aP = aS, bP = bS)
+
+# Phase shift (please see Eq. 11; Kirchner, (2016)))
+phiS_phiP <- phiS - phiP
+
+# Parameters of the gamma (transit time distribution) (please see Eqs. 10, 11; Kirchner, (2016))
+alphaBetaMTT <- findAlphaBetaMTT(phiS_phiP = phiS_phiP,
+                                 AS = AS,
+                                 AP = AP)
+
+# Age threshold of the young water fraction (please see Eq. 14; Kirchner, (2016)))
+tauyw <- findtauyw(alpha = alphaBetaMTT$alpha, method = "approximation")$tauyw
+
+# Young water fraction (equals to the amplitude ratio)
+Fyw_1 <- AS/AP
+
+# Or the young water fraction can be calculated using the lower incomplete gamma function (please see Eq. 13; Kirchner, (2016))
+Fyw_2 <- pgamma(q = tauyw,
+                shape = alphaBetaMTT$alpha,
+                scale = alphaBetaMTT$beta,
+                lower.tail = TRUE)
+
+# Show the results in data frame
+t(data.frame(AP = AP, phiP = phiP, kP = kP, AS = AS, phiS = phiS, kS = kS, 
+             Fyw_1 = Fyw_1, Fyw_2 = Fyw_2, tauyw = tauyw))
+
